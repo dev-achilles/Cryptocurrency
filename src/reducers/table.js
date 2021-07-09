e48cbb86f60e0dd4a2e3921e338434582c11b416
machete325 <machete325@gmail.com>
2021-07-09 16:32:57 +0300

    Added new case 'SORT_TABLE'

--- src/reducers/table.js
+++ src/reducers/table.js
@@ -1,4 +1,13 @@
-import { SET_TABLE_DATA } from '../constants';
+import { SET_TABLE_DATA, SORT_TABLE } from '../constants';
+
+const byField = (field, type) => {
+  if (type === 'string') {
+    return (a, b) => (a[field] > b[field] ? 1 : -1);
+  }
+  if (type === 'number') {
+    return (a, b) => (a[field] > b[field] ? 1 : -1);
+  }
+};
 
 const initialState = {
   tableData: [],
@@ -7,9 +16,19 @@ const initialState = {
 const tableReducer = (state = initialState, action) => {
   switch (action.type) {
     case SET_TABLE_DATA: {
+      const data = [];
+      Object.keys(action.payload).forEach((item) => data.push(action.payload[item]));
+      Array.from(data);
       return {
         ...state,
-        tableData: action.payload,
+        tableData: data,
+      };
+    }
+    case SORT_TABLE: {
+      const data = state.tableData.sort(byField(action.payload.field, action.payload.type));
+      return {
+        ...state,
+        tableData: data,
       };
     }
     default:
