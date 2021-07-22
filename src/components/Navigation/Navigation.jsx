c647d5be713d94f517b66123d83a6ba9b1528adb
machete325 <machete325@gmail.com>
2021-07-22 03:04:04 +0300

    Change syles for Navigation Component

--- src/components/Navigation/Navigation.jsx
+++ src/components/Navigation/Navigation.jsx
@@ -1,26 +1,44 @@
 import React from 'react';
 import { Link } from 'react-router-dom';
 import AppBar from '@material-ui/core/AppBar';
-import Toolbar from '@material-ui/core/Toolbar';
 import Typography from '@material-ui/core/Typography';
+import { makeStyles } from '@material-ui/core/styles';
+import logo from '../../assets/images/logo.jpg';
 import s from './Navigation.module.scss';
 
-const Navigation = () => (
-  <div className={s.wrapper}>
-    <AppBar position="static">
-      <Toolbar variant="dense">
-        <Typography variant="h6" color="inherit">
-          <Link to="/">Home</Link>
-        </Typography>
-        <Typography variant="h6" color="inherit">
-          <Link to="/table">Table</Link>
-        </Typography>
-        <Typography variant="h6" color="inherit">
-          <Link to="/favourite">Favourite</Link>
-        </Typography>
-      </Toolbar>
-    </AppBar>
-  </div>
-);
+const useStyles = makeStyles(() => ({
+  root: {
+    alignItems: 'center',
+    paddingRight: '20px',
+    paddingLeft: '10px',
+  },
+}));
+
+const Navigation = () => {
+  const classes = useStyles();
+
+  return (
+    <div className={s.wrapper}>
+      <AppBar className={classes.root} position="relative">
+        <div className={s.container}>
+          <div className={s.logo}>
+            <img src={logo}></img>
+          </div>
+          <div className={s.links}>
+            <Typography variant="h6" color="inherit">
+              <Link to="/">Home</Link>
+            </Typography>
+            <Typography variant="h6" color="inherit">
+              <Link to="/table">Table</Link>
+            </Typography>
+            <Typography variant="h6" color="inherit">
+              <Link to="/favourite">Favourite</Link>
+            </Typography>
+          </div>
+        </div>
+      </AppBar>
+    </div>
+  );
+};
 
 export default Navigation;
