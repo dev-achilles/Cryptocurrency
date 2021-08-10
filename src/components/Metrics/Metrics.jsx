82454f50b1142031b8613b0119a84044b8f704d0
machete325 <machete325@gmail.com>
2021-08-10 17:03:31 +0300

    Change styles for Metrics component

--- src/components/Metrics/Metrics.jsx
+++ src/components/Metrics/Metrics.jsx
@@ -1,6 +1,5 @@
 import React, { useEffect } from 'react';
 import { useParams } from 'react-router-dom';
-import Paper from '@material-ui/core/Paper';
 import Table from '@material-ui/core/Table';
 import TableBody from '@material-ui/core/TableBody';
 import TableContainer from '@material-ui/core/TableContainer';
@@ -20,38 +19,40 @@ const Metrics = (props) => {
   return (
     <div className={s.wrapper}>
       <div className={s.title}>{props.metricsData.name}</div>
-      <TableContainer component={Paper}>
-        <Table className={classes.table} aria-label="customized table">
-          <TableHead>
-            <StyledTableRow>
-              <StyledTableCell align="center">Property</StyledTableCell>
-              <StyledTableCell align="center">Value</StyledTableCell>
-            </StyledTableRow>
-          </TableHead>
-          <TableBody>
-            {props.metricsData.length !== 0 && (
-              <>
-                <StyledTableRow>
-                  <StyledTableCell align="center">Name</StyledTableCell>
-                  <StyledTableCell align="center">{props.metricsData.name}</StyledTableCell>
-                </StyledTableRow>
-                <StyledTableRow>
-                  <StyledTableCell align="center">Symbol</StyledTableCell>
-                  <StyledTableCell align="center">{props.metricsData.symbol}</StyledTableCell>
-                </StyledTableRow>
-                <StyledTableRow>
-                  <StyledTableCell styles="font-size: 20px" align="center">
-                    Supply liquid
-                  </StyledTableCell>
-                  <StyledTableCell align="center">
-                    {props.metricsData.supply.liquid ? props.metricsData.supply.liquid : 'none'}
-                  </StyledTableCell>
-                </StyledTableRow>
-              </>
-            )}
-          </TableBody>
-        </Table>
-      </TableContainer>
+      <div className="container">
+        <TableContainer>
+          <Table className={classes.table} aria-label="customized table">
+            <TableHead>
+              <StyledTableRow>
+                <StyledTableCell align="center">Property</StyledTableCell>
+                <StyledTableCell align="center">Value</StyledTableCell>
+              </StyledTableRow>
+            </TableHead>
+            <TableBody>
+              {props.metricsData.length !== 0 && (
+                <>
+                  <StyledTableRow>
+                    <StyledTableCell align="center">Name</StyledTableCell>
+                    <StyledTableCell align="center">{props.metricsData.name}</StyledTableCell>
+                  </StyledTableRow>
+                  <StyledTableRow>
+                    <StyledTableCell align="center">Symbol</StyledTableCell>
+                    <StyledTableCell align="center">{props.metricsData.symbol}</StyledTableCell>
+                  </StyledTableRow>
+                  <StyledTableRow>
+                    <StyledTableCell styles="font-size: 20px" align="center">
+                      Supply liquid
+                    </StyledTableCell>
+                    <StyledTableCell align="center">
+                      {props.metricsData.supply.liquid || 'none'}
+                    </StyledTableCell>
+                  </StyledTableRow>
+                </>
+              )}
+            </TableBody>
+          </Table>
+        </TableContainer>
+      </div>
     </div>
   );
 };
