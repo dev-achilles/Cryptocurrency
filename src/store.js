1a3f3c39f79975e682bb89b562832b9ae00f417d
machete325 <machete325@gmail.com>
2021-08-15 21:55:54 +0300

    Added new reducer user

--- src/store.js
+++ src/store.js
@@ -3,9 +3,11 @@ import { composeWithDevTools } from 'redux-devtools-extension';
 import logger from 'redux-logger';
 import thunk from 'redux-thunk';
 import tableReducer from './reducers/table';
+import userReducer from './reducers/user';
 
 const reducers = combineReducers({
   table: tableReducer,
+  user: userReducer,
 });
 
 const store = createStore(reducers, composeWithDevTools(applyMiddleware(logger, thunk)));
