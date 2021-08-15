4c0703d356fc2cda4cb2faf618c2f55190b51d5d
machete325 <machete325@gmail.com>
2021-08-15 21:55:26 +0300

    Added new constants for login

--- src/constants.js
+++ src/constants.js
@@ -1,3 +1,10 @@
+// Table constants
+
 export const GET_TABLE_DATA = 'GET_TABLE_DATA';
 export const GET_METRICS_DATA = 'GET_METRICS_DATA';
 export const SET_CHECKED_DATA = 'SET_CHECKED_DATA';
+
+// User constants
+
+export const LOGIN_USER = 'LOGIN_USER';
+export const RESET_ERROR = 'RESET_ERROR';
