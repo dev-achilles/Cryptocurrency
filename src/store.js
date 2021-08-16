a006408b6d0dc94a01495872968376a231068d0e
machete325 <machete325@gmail.com>
2021-08-17 01:16:00 +0300

    Added home reducer

--- src/store.js
+++ src/store.js
@@ -2,12 +2,14 @@ import { combineReducers, createStore, applyMiddleware } from 'redux';
 import { composeWithDevTools } from 'redux-devtools-extension';
 import logger from 'redux-logger';
 import thunk from 'redux-thunk';
+import homeReducer from './reducers/home';
 import tableReducer from './reducers/table';
 import userReducer from './reducers/user';
 
 const reducers = combineReducers({
   table: tableReducer,
   user: userReducer,
+  home: homeReducer,
 });
 
 const store = createStore(reducers, composeWithDevTools(applyMiddleware(logger, thunk)));
