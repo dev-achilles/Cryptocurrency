efc49c813bc30756cd40c5e9007000358d02d57c
machete325 <machete325@gmail.com>
2021-09-16 13:06:18 +0300

    Added types

--- src/actions/Home/types.ts
+++ src/actions/Home/types.ts
@@ -27,4 +27,28 @@ export interface getInfoDataAction {
   payload: any;
 }
 
+export type CryptocurrenciesType = {
+  name: string;
+  category: string;
+  goal: string;
+  interest: string;
+  dateActive: string;
+  column: string;
+  date: string;
+  important: string;
+  ticker: string;
+  tokenType: string;
+  tokenPrice: string;
+  fundraisingGoal: string;
+  totalTokens: string;
+  tokenSale: string;
+  roleOfToken: string;
+  links: Array<string>;
+  images: {
+    logo: string;
+    img: string;
+    video: string;
+  };
+};
+
 export type HomeAction = getHomeDataAction | setHomeDataAction | getInfoDataAction;
