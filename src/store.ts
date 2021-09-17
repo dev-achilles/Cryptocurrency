2dcb1e133871a161fbce2109fdb102fce7f54b99
machete325 <machete325@gmail.com>
2021-09-17 21:18:16 +0300

    Added register reducer

--- src/store.ts
+++ src/store.ts
@@ -5,11 +5,13 @@ import thunk from 'redux-thunk';
 import homeReducer from './reducers/home';
 import tableReducer from './reducers/table';
 import userReducer from './reducers/user';
+import registerReducer from './reducers/register';
 
 const reducers = combineReducers({
   table: tableReducer,
   user: userReducer,
   home: homeReducer,
+  register: registerReducer,
 });
 
 const store = createStore(reducers, composeWithDevTools(applyMiddleware(logger, thunk)));
